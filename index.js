9a625b8f308c779a8d070cd1e785f8b076d3c7e2
Rudy Guerrero <r.guerreronyc@gmail.com>
2020-07-24 00:34:45 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,8 +10,17 @@ class Breakfast{
 
 class Lunch{
   constructor( salad, soup, drink){
-    this.salad = salad
-    this.soup = soup
+    this.salad = salad;
+    this.soup = soup;
     this.drink = drink
+  }
+}
+
+class Dinner{
+  constructor(salad, soup, entree, dessert){
+    this.salad = salad;
+    this.soup = soup;
+    this.entree = entree;
+    this.dessert = dessert
   }
 }
